573cf59061373a51b3ed6c63ee936c70eb37ff63
Michael Kelly <mike.s.kelly@icloud.com>
2019-04-25 12:49:15 -0700

    Completed ViewModels for Users

---
+++ DotnetEcommerceStore/DotnetEcommerceStore/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetEcommerceStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+
+        }
+    }
+}
