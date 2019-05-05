26074da927c066b889a762c62f2389ea70cfa67a
iangifford <lightyagamie10@gmail.com>
2019-05-05 16:30:32 -0700

    Finishing up basket item and basket, BasketComponent, and Basket view framework

--- DotnetEcommerceStore/DotnetEcommerceStore/Components/BasketComponent.cs
+++ DotnetEcommerceStore/DotnetEcommerceStore/Components/BasketComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotnetEcommerceStore.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +7,22 @@ using System.Threading.Tasks;
 
 namespace DotnetEcommerceStore.Components
 { 
+    //Issa mini controller
     public class BasketComponent : ViewComponent
     {
+        private EComerceDbContext _context;
 
+        public BasketComponent(EComerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int number)
+        {
+            var basket = _context.Products.OrderByDescending(x => x.ID)
+                                                .Take(number)
+                                                .ToList();
+            return View(basket);
+        }
     }
 }
