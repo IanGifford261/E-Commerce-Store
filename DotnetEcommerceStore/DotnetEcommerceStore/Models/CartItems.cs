ccb4593d45ab5e0649b52fb29c3516769bc93370
iangifford <lightyagamie10@gmail.com>
2019-05-07 11:10:32 -0700

    Finished casket controller crud ops

--- DotnetEcommerceStore/DotnetEcommerceStore/Models/CartItems.cs
+++ DotnetEcommerceStore/DotnetEcommerceStore/Models/CartItems.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,25 @@ namespace DotnetEcommerceStore.Models
 {
     public class CartItems
     {
-        public int ID { get; set; }
         public int CartID { get; set; }
-        public int Quantity { get; set; }
         public int ProductID { get; set; }
-
+        [Range(0, 20, ErrorMessage = "Please Enter A Desired Purchase Quantity between 0 and 20.")]
+        public int Quantity { get; set; }
+        public Cart Cart { get; set; }
         public Product Product { get; set; }
+
+
+        //        public int CartID { get; set; }
+        //        public int ProductID { get; set; }
+        //        public string Name { get; set; }
+        //        public string SKU { get; set; }
+        //        public string Img { get; set; }
+        //        public string Description { get; set; }        
+        //        [Range(0, 20, ErrorMessage = "Please Enter A Desired Purchase Quantity between 0 and 20.")]
+        //        public int Quantity { get; set; }
+        //        public decimal Price { get; set; }
+        //        public decimal ProductTotal { get; set; }
+        //        public decimal TotalCost { get; set; }
+        //        public Product Product { get; set; }
     }
 }
