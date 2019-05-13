2fadaa81f89837254d0aa826238d0bbea637c012
iangifford <lightyagamie10@gmail.com>
2019-05-13 10:02:08 -0700

    moving view component around

--- DotnetEcommerceStore/DotnetEcommerceStore/Migrations/EComerceDb/20190510213841_restart.Designer.cs
+++ DotnetEcommerceStore/DotnetEcommerceStore/Migrations/EComerceDb/20190510213841_restart.Designer.cs
@@ -4,14 +4,16 @@ using DotnetEcommerceStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DotnetEcommerceStore.Migrations.EComerceDb
 {
     [DbContext(typeof(EComerceDbContext))]
-    partial class EComerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190510213841_restart")]
+    partial class restart
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
